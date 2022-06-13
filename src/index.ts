37ebe479e21d86fcb7577205d2809da901efb10f
Ars8 <pogosov-91@mail.ru>
2022-06-13 10:25:56 +0300

    test67

--- src/index.ts
+++ src/index.ts
@@ -166,21 +166,7 @@ app.put('/posts/:id', (req: Request, res: Response) => {
     let content = req.body.content
     let bloggerId = req.body.bloggerId
 
-    if (!title || typeof title !== 'string' || !title.trim() || title.length > 30 && shortDescription.length > 100 || title.length > 30) {
-        res.status(400).send({
-            errorsMessages: [{
-                'message': 'Incorrect shortDescription',
-                'field': 'shortDescription'
-            },
-                {
-                    'message': 'Incorrect name',
-                    'field': 'title'
-                }]
-        })
-        return
-    }
-
-    if (!title || typeof title !== 'string' || !title.trim() || title.length > 30 && content.length > 1000 || title.length > 30) {
+    if (!title || typeof title !== 'string' || !title.trim() || title.length > 30 || title.length > 30 && content.length > 1000) {
         res.status(400).send({
             errorsMessages: [{
                 'message': 'Incorrect title',
@@ -189,6 +175,20 @@ app.put('/posts/:id', (req: Request, res: Response) => {
                 {
                     'message': 'Incorrect content',
                     'field': 'content'
+                }]
+        })
+        return
+    }
+
+    if (!title || typeof title !== 'string' || !title.trim() || title.length > 30 || title.length > 30 && shortDescription.length > 100) {
+        res.status(400).send({
+            errorsMessages: [{
+                'message': 'Incorrect shortDescription',
+                'field': 'shortDescription'
+            },
+                {
+                    'message': 'Incorrect title',
+                    'field': 'title'
                 }]
         })
         return
