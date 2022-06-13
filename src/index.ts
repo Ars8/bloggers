72313c86a2b2631e38663c9c2d35cb5ac80cea14
Ars8 <pogosov-91@mail.ru>
2022-06-13 09:28:02 +0300

    test57

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,27 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-interface bloggers {
+interface blogger {
     id: number,
     name: string,
     youtubeUrl: string
 }
 
-let bloggers: bloggers[] = [
+interface post {
+    id: number,
+    title: string,
+    shortDescription: string,
+    content: string,
+    bloggerId: number,
+    bloggerName: string
+}
+
+let bloggers: blogger[] = [
     {id: 1, name: 'About JS - 01', youtubeUrl: 'it-incubator.eu'},
+]
+
+let posts: post[] = [
+    {id: 1, title: 'About', shortDescription: 'About JS - 01', content: 'it-incubator.eu', bloggerId: 1, bloggerName: 'About'},
 ]
 
 const parserMiddleware = bodyParser({})
@@ -85,6 +98,87 @@ app.put('/bloggers/:id', (req: Request, res: Response) => {
     }
 })
 app.delete('/bloggers/:id', (req: Request, res: Response) => {
+    const id = +req.params.id
+
+    if (!id) {
+        res.status(404)
+    }
+
+    const newBloggers = bloggers.filter(item => { return item.id !== id })
+
+    if (newBloggers.length < bloggers.length) {
+        bloggers = newBloggers
+        res.send(204)
+    } else {
+        res.send(404)
+    }
+})
+
+app.get('/posts', (req: Request, res: Response) => {
+    res.status(200).send(posts)
+})
+app.get('/posts/:id', (req: Request, res: Response) => {
+    const id = +req.params.id
+    if(typeof id !== 'number') {
+        res.send(400)
+        return
+    }
+    const post = posts.find(p => p.id === id)
+    if (post) {
+        res.status(200).send(post)
+    }else {
+        res.send(404)
+    }
+})
+app.post('/posts', (req: Request, res: Response) => {
+    let name = req.body.name
+    if (!name || name == null ||  typeof name !== 'string' || !name.trim() || name.length > 15) {
+        res.status(400).send({
+            errorsMessages: [{
+                'message': 'Incorrect name',
+                'field': 'name'
+            }]
+        })
+        return
+    }else {
+        const newBlogger = {
+            id: +(new Date()),
+            name: req.body.name,
+            youtubeUrl: req.body.youtubeUrl
+        }
+        bloggers.push(newBlogger)
+        res.status(201).send(newBlogger)
+    }
+})
+app.put('/posts/:id', (req: Request, res: Response) => {
+    let name = req.body.name
+    let youtubeUrl = req.body.youtubeUrl
+    if (!name || typeof name !== 'string' || !name.trim() || name.length > 40) {
+        res.status(400).send({
+            errorsMessages: [
+                {
+                    'message': 'Incorrect youtubeUrl',
+                    'field': 'youtubeUrl'
+                },
+                {
+                    'message': 'Incorrect name',
+                    'field': 'name'
+                }
+            ]
+        })
+        return
+    }
+    const id = +req.params.id
+    const blogger = bloggers.find(item => item.id === id)
+    if (blogger) {
+        blogger.name = name,
+            blogger.youtubeUrl = youtubeUrl
+        res.status(204).send(bloggers)
+    } else {
+        res.send(404)
+    }
+})
+app.delete('/posts/:id', (req: Request, res: Response) => {
     const id = +req.params.id
 
     if (!id) {
