a60e4dab63792755340916322c0a44d68d57d513
Ars8 <pogosov-91@mail.ru>
2022-07-07 13:07:41 +0300

    lglglg

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -1,10 +1,33 @@
 import {Request, Response, Router} from "express";
 import {usersService} from "../domain/users-service";
 import {jwtService} from "../application/jwt-service";
+import {body, validationResult} from "express-validator";
 
 export const authRouter = Router({})
 
-authRouter.post('/login', async(req: Request, res: Response) => {
+export const usersLoginValidation = body('login')
+    .exists().withMessage('incorrect login')
+    .trim().notEmpty().withMessage('incorrect login')
+    .isString().withMessage('incorrect login')
+
+export const usersPasswordValidation = body('password')
+    .exists().withMessage('incorrect password')
+    .trim().notEmpty().withMessage('incorrect password')
+    .isString().withMessage('incorrect password')
+
+authRouter.post('/login', usersLoginValidation, usersPasswordValidation, async(req: Request, res: Response) => {
+
+    const err = validationResult(req)
+    const errors = err.array({ onlyFirstError: true }).map(elem => {
+        return {
+            message: elem.msg,
+            field: elem.param,
+        }
+    })
+    if (!err.isEmpty()) {
+        return res.status(400).json({ errorsMessages: errors })
+    }
+
     const user = await usersService.checkCredentials(req.body.login, req.body.password)
     if (user) {
         const token = await jwtService.createJWT(user)
