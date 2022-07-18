9ed70a13245cfed0b1b9a2c3d8cfc183d3e47186
Ars8 <pogosov-91@mail.ru>
2022-07-18 16:49:25 +0300

    hbnnfvjfmdmc

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -35,7 +35,7 @@ const userEmailValidation = body('email')
     .exists().withMessage('incorrect email')
     .trim().notEmpty().withMessage('incorrect email')
     .isString().withMessage('incorrect email')
-    .isEmail().withMessage('incorrect email by regex part 4')
+    .normalizeEmail().isEmail()
     .custom(email => {
         return authService.checkEmail(email).then(function(email) {
             if (email) {
