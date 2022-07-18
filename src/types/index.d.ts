f00175d4f88a51165c6000640b5f867a8c46143c
Ars8 <pogosov-91@mail.ru>
2022-07-18 10:02:48 +0300

    plmkjnhytg

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,9 @@
-import {UserDBType} from "../repositories/types";
+import {UserAccountDBType, UserDBType} from "../repositories/types";
 
 declare global {
     declare namespace Express {
         export interface Request {
-            user: UserDBType | null
+            user: UserAccountDBType | null
         }
     }
 }
