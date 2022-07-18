809787fd673c9c42a52df24fee29f9665964fc58
Ars8 <pogosov-91@mail.ru>
2022-07-18 18:51:33 +0300

    hgbvfvgfvgf

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -51,7 +51,7 @@ const EmailValidation = body('email')
     .isEmail().withMessage('Eto email')
     .custom(async email => {
         return await authService.checkIsConfirmed(email).then(function(user) {
-            if (!user || user?.emailConfirmation.isConfirmed === true) {
+            if (user && user?.emailConfirmation.isConfirmed === true) {
                 throw new Error('this email is already confirm1')
             }
         }
