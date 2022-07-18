b121d4612daa1b02790c16ed1092bdc543259349
Ars8 <pogosov-91@mail.ru>
2022-07-18 13:32:43 +0300

    oiuhfbvncmd

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -11,7 +11,7 @@ export const emailAdapter = {
           });
         
           let info = await transporter.sendMail({
-            from: '"Fransua bloggers" <fransuazakubov@gmail.com>',
+            from: '"Fransua blogger" <fransuazakubov@gmail.com>',
             to: email,
             subject: subject,
             html: message,
