5cf68d515e97bb834ad8bebfd8133b45f0311f0a
Ars8 <pogosov-91@mail.ru>
2022-07-19 11:43:05 +0300

    jnhbgvwqwqwq

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -14,7 +14,7 @@ export const usersLoginValidation = body('login')
     .isLength({ min: 3, max: 10 }).withMessage('incorrect login')
     .custom(async login => {
         return await authService.checkLogin(login).then(function(login) {
-            if (!login) {
+            if (login) {
                 throw new Error('cannot find login')
             }
         }
