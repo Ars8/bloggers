baa3180fbf842a2d6cb63ce6bb1a8be54a65faf6
Ars8 <pogosov-91@mail.ru>
2022-07-25 14:36:57 +0300

    jnhygvfbdgfbv

--- src/repositories/tokens-repository.ts
+++ src/repositories/tokens-repository.ts
@@ -4,11 +4,10 @@ export const tokensRepository = {
     async saveToken(id: string, refreshToken: string) {
         const tokenData = await tokensCollection.findOne({id: id}, {projection: {_id: 0}})
         if (tokenData) {
-            tokenData.refreshToken = refreshToken;
-            return
+            await tokensCollection.insertOne({id: id, refreshToken})
+            return {id, refreshToken}
         }
-        await tokensCollection.insertOne({id: id, refreshToken})
-        return {id, refreshToken}
+        
     },
     async findRefreshToken(refreshToken: string) {
         const tokenData = await tokensCollection.findOne({refreshToken: refreshToken}, {projection: {_id: 0}})
